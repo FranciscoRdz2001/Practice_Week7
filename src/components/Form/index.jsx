05256e66c6a8e48264161b1efcf7f5646578f383
CATAAA233 <carlosdanielcatalan1o@gmail.com>
2023-03-14 04:51:12 -0700

    form structure

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,6 +1,20 @@
 const Form =()=>{
     return(
-        <div>this is a list</div>
+        <form className="Form__Container" action="">
+            <label htmlFor="">
+                Clave
+            <input placeholder="Ejm. AEB-1011" type="text" />
+            </label>
+            <label htmlFor="">
+                Materia
+            <input placeholder="Ejm. Desarrollo de ..." type="text" />
+            </label>
+            <label htmlFor="">
+                Calificación
+            <input placeholder="Ejm. 75" type="text" />
+            </label>
+            <button type="submit">Enviar</button>
+        </form>
     );
 }
 export default Form;
